aa722dd069701b7b255b3cb103055dbd311cdfea
1lch2 <1lch2@163.com>
2022-03-15 20:08:58 +0800

    update leetcode

--- src/leetcode/linkedList/143.js
+++ src/leetcode/linkedList/143.js
@@ -39,29 +39,60 @@ var reorderList = function(head) {
   if(head.next == null || head.next.next == null) {
     return head;
   }
+  
+  /**
+   * @param {ListNode} head 
+   * @returns {ListNode}
+   */
+  const findMiddle = (head) => {
+    let fast = head;
+    let slow = head;
+    
+    // 双指针定位中点
+    while(fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+    }
+    // 此时 slow.next 指向中点（奇数长度）或中点前驱（偶数长度）
+    return slow;
+  };
+  
+  /**
+   * @param {ListNOde} head 
+   */
+  const reverse = (head) => {
+    let prev = null;
+    let curr = head;
+    let temp;
+    while(curr != null) {
+      temp = curr;
+      curr = curr.next;
+      temp.next = prev;
+      prev = temp;
+    }
+    return prev;
+  };
 
   let dummy = new ListNode();
   dummy.next = head;
+  
+  let mid = findMiddle(head);
+  let reversedHead = reverse(mid.next);
+  mid.next = null;
 
-  let fast = head;
-  let slow = head;
+  // 合并两个链表
+  let l1 = head;
+  let l2 = reversedHead;
+  while(l1 != null && l2 != null) {
+    let temp1 = l1.next;
+    let temp2 = l2.next;
 
-  while(fast != null && fast.next != null) {
-    fast = fast.next.next;
-    slow = slow.next;
+    l1.next = l2;
+    l1 = temp1;
+
+    l2.next = l1;
+    l2 = temp2;
   }
-
-  // 此时 slow.next 指向中点（奇数长度）或中点前驱（偶数长度）
-  // 将中点后的节点按从后往前的顺序插到前半部分
-  let front = head;
-  let back = slow;
-
-  /**
-   * @param {ListNode} head 
-   */
-  const traverseBack = (head) => {
-    // TODO:
-  };
 
   return dummy.next;
 };
