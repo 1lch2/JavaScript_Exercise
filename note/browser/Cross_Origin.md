3dc7deef0a86137b905817faf999dfd55571781d
1lch2 <1lch2@163.com>
2022-08-02 19:17:54 +0800

    update notes

--- note/browser/Cross_Origin.md
+++ note/browser/Cross_Origin.md
@@ -87,3 +87,41 @@ JSONP, a.k.a. JSON with Padding。
 Cross-Origin Resource Sharing，是跨源AJAX请求的根本解决方法。相比JSONP只能发GET请求，CORS允许任何类型的请求。
 
 CORS使用额外的 HTTP 头来告诉浏览器 让运行在一个 origin (domain) 上的Web应用被准许访问来自不同源服务器上的指定的资源。当一个资源从与该资源本身所在的服务器不同的域、协议或端口请求一个资源时，资源会发起一个跨域 HTTP 请求。
+
+#### 服务器端
+对于跨域请求，服务器需要设置响应头来允许请求跨域，常见请求头设置如下：
+
+```http
+Access-Control-Allow-Credentials: true
+Access-Control-Allow-Headers: content-type
+Access-Control-Allow-Headers: Authorization
+Access-Control-Allow-Methods: POST, OPTIONS, GET
+Access-Control-Allow-Origin: http://127.0.0.1:5500
+Access-Control-Expose-Headers: Authorization
+Access-Control-Max-Age: 86400
+```
+
+以上请求头解释如下：
+- `Access-Control-Allow-Credentials`：允许跨域请求携带凭证，如cookies。在这项为 `true` 时，`Access-Control-Allow-Origin` 就不能设置为通配符`"*"`，而要指定允许的请求源
+- `Access-Control-Allow-Headers`：用于 preflight 请求中，列出了将会在正式请求的 Access-Control-Request-Headers 字段中出现的首部信息。常见的简单header包括在其中不需要指定，但通常会用来携带 token的 `Authorization` 头不在其中，必须要在这里指定。
+    - 注意：`Content-Type`虽然默认包括在内，但是不包括解析后值为`application/json`的情况，传递JSON数据时还是需要专门添加这一项
+- `Access-Control-Allow-Methods`：将所有允许的请求方法告知客户端
+- `Access-Control-Expose-Headers`：列出了哪些首部可以作为响应的一部分暴露给外部。前端发起携带token的求时需要指定这个 header
+- `Access-Control-Max-Age`：表示 preflight request （预检请求）的返回结果（即 Access-Control-Allow-Methods 和Access-Control-Allow-Headers 提供的信息） 可以被缓存多久。
+
+#### 客户端
+对于 fetch API，在使用JWT，并将token存放在 localstorage的情况下，发起请求时需要指定跨域模式，并携带token。示例如下：
+```js
+fetch(url, {
+  headers: {
+    "Authorization": localStorage.getItem("token")
+  },
+  mode: "cors"
+})
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+  }).catch(err => {
+    console.log(err);
+  });
+```
--- src/functions/parseUrlParams.js
+++ src/functions/parseUrlParams.js
@@ -9,7 +9,7 @@ function parseParam(url) {
   const paramsStr = /.+\?(.+)$/.exec(url)[1]; // 将 ? 后面的字符串取出来
   const paramsArr = paramsStr.split("&"); // 将字符串以 & 分割后存到数组中
   let paramsObj = {};
-  
+
   // 将 params 存到对象中
   paramsArr.forEach(param => {
     if (/=/.test(param)) { // 处理有 value 的参数
@@ -29,3 +29,42 @@ function parseParam(url) {
 
   return paramsObj;
 }
+
+/**
+ * my implementation
+ * 
+ * extract query string as json object from an url
+ * @param {String} url url containing query string
+ */
+const parseQueryString = function(url) {
+  if (typeof (url) !== "string") {
+    return {};
+  }
+
+  let queryIndex = url.indexOf("?");
+  if (queryIndex === -1) {
+    return {};
+  }
+
+  let queryString = url.split("?")[1];
+  let shouldDecodeUrl = queryString.indexOf("%") !== -1;
+  if (shouldDecodeUrl) {
+    queryString = decodeURIComponent(queryString);
+  }
+
+  let result = {};
+  // check if value is integer
+  let regex = /^[0-9]*$/;
+  let params = queryString.split("&");
+  for (let each of params) {
+    let pair = each.split("=");
+    result[pair[0]] = regex.test(pair[1]) ? parseInt(pair[1], 10) : pair[1];
+  }
+
+  return result;
+};
+
+// test case
+let input = "https://steamcommunity.com/workshop/browse/?appid=431960&searchtext=%E6%98%8E%E6%97%A5%E6%96%B9%E8%88%9F&childpublishedfileid=0&browsesort=totaluniquesubscribers&section=readytouseitems&requiredtags%5B0%5D=Everyone&excludedtags%5B0%5D=MMD&created_date_range_filter_start=0&created_date_range_filter_end=0&updated_date_range_filter_start=0&updated_date_range_filter_end=0&actualsort=totaluniquesubscribers&p=5";
+
+console.log(parseQueryString(input));
