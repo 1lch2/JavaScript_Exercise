3297a57673980482ff91bcae5c1d88ce546c64c3
1lch2 <1lch2@163.com>
2022-12-10 16:29:11 +0800

    update leetcode

--- src/leetcode/greedy/1775.通过最少操作次数使数组的和相等.js
+++ src/leetcode/greedy/1775.通过最少操作次数使数组的和相等.js
@@ -47,13 +47,48 @@ var minOperations = function(nums1, nums2) {
     return 0;
   }
 
+  const LEN1 = nums1.length;
+  const LEN2 = nums2.length;
+
   // 无法满足条件
-  if (nums1.length > nums2.length * 6 || nums2.length > nums1.length * 6) {
+  if (LEN1 > LEN2 * 6 || LEN2 > LEN1 * 6) {
     return -1;
   }
 
-
   let opsCount = 0;
-  // 贪心策略，找到最小值增大到最大
-  // TODO:
+  // 保证nums1之和大于nums2
+  if(delta < 0) {
+    [nums1, nums2] = [nums2, nums1];
+    delta = -delta;
+  }
+
+  // nums1 降序，nums2 升序
+  nums1.sort((a, b) => b - a);
+  nums2.sort((a, b) => a - b);
+
+  // 双指针
+  // 贪心思路：每次选差值最大的一边进行改动
+  let i = 0; 
+  let j = 0;
+  while((i < LEN1 || j < LEN2) && delta > 0) {
+    let diff1 = nums1[i] - 1;
+    let diff2 = 6 - nums2[j];
+
+    if(diff1 >= diff2) {
+      delta -= diff1;
+      i++;
+    } else {
+      delta -= diff2;
+      j++;
+    }
+    opsCount++;
+  }
+
+  return opsCount;
 };
+
+let input = [
+  [6, 6],
+  [1]
+];
+console.log(minOperations(...input)); // 3
