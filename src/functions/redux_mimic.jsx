47cbaa7688160f01b8a0bddf36a86174665bdb16
1lch2 <1lch2@163.com>
2022-12-24 15:26:02 +0800

    update leetcode

--- src/functions/redux_mimic.jsx
+++ src/functions/redux_mimic.jsx
@@ -2,11 +2,11 @@ import React, { memo, createContext, useReducer, useContext } from "react";
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case "add": return { count: state.count + 1 }
-    case "sub": return { count: state.count > 0 ? state.count - 1 : 0 }
-    case "reset": return { count: action.payload ? action.payload : 0 }
+    case "add": return { count: state.count + 1 };
+    case "sub": return { count: state.count > 0 ? state.count - 1 : 0 };
+    case "reset": return { count: action.payload ? action.payload : 0 };
   }
-}
+};
 
 // 创建context
 const Context = createContext(null);
@@ -23,11 +23,11 @@ const ContextProvider = (props) => {
 };
 
 const Show = () => {
-  const { state } = useContext(Context)
+  const { state } = useContext(Context);
   return (
-      <div>当前数字是：{state.count}</div>
-  )
-}
+    <div>当前数字是：{state.count}</div>
+  );
+};
 
 const UI = memo(
   (props) => {
@@ -37,32 +37,32 @@ const UI = memo(
 );
 
 const Increase = () => {
-  const { dispatch } = useContext(Context)
-  return <UI dispatch={dispatch}/>
-}
+  const { dispatch } = useContext(Context);
+  return <UI dispatch={dispatch}/>;
+};
 
 const Decrease = () => {
-  console.log("Decrease")
-  const { dispatch } = useContext(Context)
-  return <button onClick={() => dispatch({ type: "sub" })}> - </button>
-}
+  console.log("Decrease");
+  const { dispatch } = useContext(Context);
+  return <button onClick={() => dispatch({ type: "sub" })}> - </button>;
+};
 
 const Reset = memo(() => {
-  console.log("Reset")
-  const { dispatch } = useContext(Context)
-  const initialValue = 8
-  return <button onClick={() => dispatch({ type: "reset", payload: initialValue })}> 重置 </button>
-})
+  console.log("Reset");
+  const { dispatch } = useContext(Context);
+  const initialValue = 8;
+  return <button onClick={() => dispatch({ type: "reset", payload: initialValue })}> 重置 </button>;
+});
 
 const App = () => {
   return (
-      <ContextProvider>
-          <Show />
-          <Increase />
-          <Decrease />
-          <Reset />
-      </ContextProvider>
-  )
-}
+    <ContextProvider>
+      <Show />
+      <Increase />
+      <Decrease />
+      <Reset />
+    </ContextProvider>
+  );
+};
 
 export default App;
